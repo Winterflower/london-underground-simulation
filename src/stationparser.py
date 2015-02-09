10e97b4d59ddc69c7d43adb5dcf23caa26e45a94
Winterflower <camillamon@gmail.com>
2015-02-09 21:18:26 +0000

    Stationparser update

--- src/stationparser.py
+++ src/stationparser.py
@@ -3,6 +3,8 @@ __author__ = 'winterflower'
 import station
 import graph
 import re
+from graph_tool.all import *
+from graph_tool.topology import shortest_path
 
 def parse_graph(fileobject):
     graphoject=graph.Graph()
@@ -14,7 +16,7 @@ def parse_graph(fileobject):
             temp_neighbour_repo.append(line_elements[1])
             temp_node=graph.Node(station_name,[])
             graphoject.addnode(temp_node)
-    for i in range(len(graphoject.nodes)):cd Dow
+    for i in range(len(graphoject.nodes)):
         neighbour_nodes=[]
         neighbour_names=temp_neighbour_repo[i].split(',')
         for name in neighbour_names:
@@ -32,3 +34,29 @@ for node in tubemap.nodes:
 
 
 
+def parse_graph_forvisualization(graphobject):
+    graph=Graph()
+    mapping={}
+    for vertex in graphobject.nodes:
+        node=graph.add_vertex()
+        mapping[vertex.name]=node
+    #create the neighbours
+    for station in mapping.keys():
+        newnode=mapping[station]
+        oldnode=graphobject.lookupnode(station)
+        for neighbour in oldnode.neighbours:
+            graph.add_edge(newnode,mapping[neighbour.name])
+    return graph, mapping
+
+graph, mapping=parse_graph_forvisualization(tubemap)
+
+
+print "Finding the shortest path"
+
+
+vertices, edges=shortest_path(graph, mapping["Baker Street"], mapping["Aldgate"])
+print vertices
+
+
+
+#graph_draw(graph)
