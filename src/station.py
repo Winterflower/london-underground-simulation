ca17e8c41269d32a9b2762c1f5cf334c1e5d7f24
Winterflower <camillamon@gmail.com>
2015-03-02 01:38:31 +0000

    Added commuters to the STation object

--- src/station.py
+++ src/station.py
@@ -3,16 +3,25 @@ Represents the station object
 Learned about the __eq_method from http://stackoverflow.com/questions/390250/elegant-ways-to-support-equivalence-equality-in-python-classes
 """
 
+import numpy as np
+try:
+    import commuter
+except:
+    print "Please make sure the commuter module is imported and configured correctly"
+
 class Station():
-  def __init__(self, name, nearest_stations):
-    self.name=name
-    self.nearest_stations=nearest_stations
-  def getName(self):
-    return self.name
-  def getNearestStations(self):
-    return self.nearest_stations
-  def __eq__(self,other):
-    if isinstance(other, self.__class__):
-      return self.__dict__==other.__dict__
-    else:
-      return False
+    def __init__(self, name):
+        self.name=name
+        self.number_of_commuters=0
+    def add_neighbours(self, neighbours):
+        self.neighbours=neighbours
+    def set_number_of_commuters(self, number):
+        self.number_of_commuters=number
+    def create_initial_commuters(self, target_stations):
+        self.commuters=[]
+        for i in range(self.number_of_commuters):
+            self.commuters.append(commuter.Commuter(self, target_stations[i]))
+    def set_vertex_descriptor(self,vertex_descriptor):
+        self.vertex_descriptor=vertex_descriptor
+
+
