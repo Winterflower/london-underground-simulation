32ef840fe895dea31f995d407281f8a97d647ee7
Winterflower <camillamon@gmail.com>
2015-03-02 01:37:38 +0000

    Major changes to parsedata.py

--- src/parsedata.py
+++ src/parsedata.py
@@ -7,7 +7,7 @@ from graph_tool.all import *
 def parse_file(filename):
     """
     :param filename:
-    :return: list of Station objects
+    :return: list of Station objects which can be further manipulated to desired object
     """
     datafile=open(filename,'r')
     #a repository for all stations in the map
@@ -26,20 +26,66 @@ def parse_file(filename):
             station.add_neighbours(neighbours)
             stations.append(station)
     return stations
-def lookupstation(listofstations, station_name):
+def lookupstationinlist(listofstations, station_name):
     """
 
     :param listofstations:
     :param station_name:
     :return:
-    Returns the station object is a station with that name exists, otherwise returns -1
+    Returns the station object is a station with that name exists, otherwise returns -1Winte
     """
     found=False
     for station_object in listofstations:
-        if station_object.name == listofstations:
+        if station_object.name == station_name:
             found=True
             return station_object
     if not found:
         return -1
 
 
+def createmap(list_of_station_objects):
+    graphobject=Graph()
+    name_index={}
+    #create a property map to store stations
+    station_propertymap=graphobject.new_vertex_property("object")
+    #iterate through all stations:
+    # 1. get the vertex descriptor and add Station object and descriptor to property map
+    for station in list_of_station_objects:
+        vertex=graphobject.add_vertex()
+        station_propertymap[vertex]=station
+        name_index[station.name]=vertex
+        station.set_vertex_descriptor(vertex)
+    return graphobject, station_propertymap,name_index, list_of_stations
+
+
+def create_edges(graphobject, list_of_station_objects,name_index):
+    #This method assumes that the graphobject has been created
+    # It is a bit hacky, but it is an easy way to guarantee
+    #that all vertices exist when we want to add an edge
+    #iterate through the neighbours of the station and add each to the map
+    edge_propertymap=graphobject.new_edge_property("string")
+    for station in list_of_station_objects:
+        source_name=station.name
+        source_vertex=name_index[source_name]
+        neighbours_dict=station.neighbours
+        for neighbour_station in neighbours_dict.keys():
+            #lookup the neighbouring station and add to graph
+            target_name=neighbour_station #the dict contains a string
+            target_vertex=name_index[target_name]
+            lines=neighbours_dict[neighbour_station]
+            for tubeline in lines:
+                edge=graphobject.add_edge(source_vertex,target_vertex)
+                edge_propertymap[edge]=tubeline
+    return graphobject, edge_propertymap
+
+############
+#Some test code
+##############
+
+list_of_stations=parse_file('data/londontubes.txt')
+graph, station_prop_map, name_index, list_of_stations = createmap(list_of_stations)
+finalgraph, edge_props = create_edges(graph, list_of_stations,name_index)
+
+for edge in name_index["Acton Town"].out_edges():
+    print(edge)
+    print edge_props[edge]
--- src/test_parsedata.py
+++ src/test_parsedata.py
@@ -1,6 +1,7 @@
 __author__ = 'winterflower'
 import parsedata
 import unittest
+from graph_tool.all import *
 
 class TestParseData(unittest.TestCase):
     def setUp(self):
@@ -16,7 +17,9 @@ class TestParseData(unittest.TestCase):
     def test_parsefilegetscorrectneighbours(self):
         self.stations=parsedata.parse_file(self.testdatafile)
         #get the neighbours of "Acton Town"
-        neighbours=self.stations[0].neigbours
+        neighbours=self.stations[0].neighbours
+        print "Neighbour"
+        print neighbours
         self.assertDictEqual(neighbours, {"Chiswick Park":["District"],
                                           "South Ealing":["Picadilly"],
                                            "Turnham Green":["Picadilly"],
@@ -27,8 +30,27 @@ class TestGraphGeneration(unittest.TestCase):
         self.testdatafile="test_data.txt"
         self.stations=parsedata.parse_file(self.testdatafile)
     def test_stationlookupreturnscorrectstation(self):
-        station=parsedata.lookupstation(self.stations,"Acton Town")
+        station=parsedata.lookupstationinlist(self.stations,"Acton Town")
         self.assertEquals(station.name, "Acton Town")
     def test_stationlookupreturnsminusoneifnotfound(self):
-        station=parsedata.lookupstation(self.stations, "High Street Kensington")
+        station=parsedata.lookupstationinlist(self.stations, "High Street Kensington")
         self.assertEquals(station,-1)
+    def test_graphobjecthascorrectnumberofvertices(self):
+        self.graph_object, self.station_propertymap = parsedata.createmap(self.stations)
+        vertices=[]
+        for vertex in self.graph_object.vertices():
+            vertices.append(vertex)
+        self.assertEquals(4, len(vertices))
+    def test_propertymapmapstotherightstationobjects(self):
+        #create a list of vertex names expected based on the test data
+        test_vertices=["Acton Town", "Aldgate", "Aldgate East", "Alperton"]
+        vertex_counter=0
+        graph_object, station_propertymap = parsedata.createmap(self.stations)
+        for vertex in graph_object.vertices():
+            mapped_station_object=station_propertymap[vertex]
+            #vertices are returned in the order they are added accroding to the docs
+            #this test relies on the above property
+            print mapped_station_object.name
+            self.assertEquals(test_vertices[vertex_counter], mapped_station_object.name)
+            vertex_counter+=1
+
