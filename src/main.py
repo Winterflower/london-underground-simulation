2ca3f411dedeae08f8b0f47e8ff93205920e83f7
Winterflower <camillamon@gmail.com>
2015-03-11 21:58:05 +0000

    Some new tests for registering commuters on stations

--- src/main.py
+++ src/main.py
@@ -29,19 +29,43 @@ def create_random_target_stations(map, number_of_commuters):
         target_stations.append(station)
     return target_stations
 
+
+
 def initialise_commuters(map, list_of_targets):
     for vertex in map.graph_object.vertices():
         initial_commuter_number=np.random.random_integers(1,4000)
         station_object=map.station_property_map[vertex]
         station_object.set_number_of_commuters=initial_commuter_number
-        station_object.create_commuter_objects(list_of_targets)
-
+        station_object.create_initial_commuters(list_of_targets, map)
 
 def simulate_one_cycle(map):
     #each commuter calculates the shortest distance to his target
     for vertex in map.graph_object.vertices():
         station=map.station_property_map[vertex]
         for commuter in station.commuters:
-            shortest_path_vertices=shortest_path(commuter.source.vertex_descriptor, commuter.targer.vertex_descriptor)
+            commuter.travel_to_next_station()
+
+def main(map):
+    graph_object=map.graph_object
+    target_stations=create_random_target_stations(map, 40000)
+    initialise_commuters(map, target_stations)
+    print "Liverpool Station"
+    vertex=map.station_name_index["Aldgate"]
+    station_object=map.station_property_map[vertex]
+    print station_object.number_of_current_commuters
+    simulate_one_cycle(map)
+    vertex=map.station_name_index["Baker Street"]
+    station_object=map.station_property_map[vertex]
+    print station_object.number_of_current_commuters
+    for vertex in map.graph_object.vertices():
+        station_object=map.station_property_map[vertex]
+        print station_object.name
+        print station_object.number_of_current_commuters
+
+main(map)
+
+
+
+
 
 
