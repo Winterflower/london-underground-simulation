fa90fc469cd28dc0f206df80198d5c4223b47b8b
Winterflower <camillamon@gmail.com>
2015-03-22 01:18:57 +0000

    Finally working version of simulation

--- src/main.py
+++ src/main.py
@@ -5,17 +5,15 @@ This is the main control center of the simulation
 """
 import time
 import numpy as np
-from map import Map
+import simulation_utils
 from graph_tool.topology import *
 
 ##########################
 #Initial objects needed
 ##########################
-print "Creating Tube Map"
-map=Map('data/londontubes.txt')
-map.initialise_map()
 
-def create_random_target_stations(map, number_of_commuters):
+
+def create_random_target_station_objects(map,source_station_object, number_of_commuters):
     """
     Creates a random list of target stations for commuter objects to use
     :param map:
@@ -26,17 +24,22 @@ def create_random_target_stations(map, number_of_commuters):
         #-1, because the low and high are inclusive in np.random.random_integers
         vertex=map.graph_object.vertex(np.random.random_integers(0,map.graph_object.num_vertices()-1))
         station=map.station_property_map[vertex]
+        while station.name==source_station_object.name:
+            vertex=map.graph_object.vertex(np.random.random_integers(0,map.graph_object.num_vertices()-1))
+            station=map.station_property_map[vertex]
         target_stations.append(station)
     return target_stations
 
 
 
-def initialise_commuters(map, list_of_targets):
+def initialise_commuters(map):
     for vertex in map.graph_object.vertices():
-        initial_commuter_number=np.random.random_integers(1,4000)
+        initial_commuter_number=np.random.random_integers(1,40)
         station_object=map.station_property_map[vertex]
-        station_object.set_number_of_commuters=initial_commuter_number
-        station_object.create_initial_commuters(list_of_targets, map)
+        list_of_target_station_objects=create_random_target_station_objects(map, station_object,initial_commuter_number)
+        print station_object.name
+        station_object.set_number_of_initial_commuters(initial_commuter_number)
+        station_object.create_initial_commuters(list_of_target_station_objects, map)
 
 def simulate_one_cycle(map):
     #each commuter calculates the shortest distance to his target
@@ -45,24 +48,30 @@ def simulate_one_cycle(map):
         for commuter in station.commuters:
             commuter.travel_to_next_station()
 
-def main(map):
+def main():
+    print "Creating Tube Map"
+    map=simulation_utils.Map('data/londontubes.txt')
+    map.initialise_map()
     graph_object=map.graph_object
-    target_stations=create_random_target_stations(map, 40000)
-    initialise_commuters(map, target_stations)
-    print "Liverpool Station"
+    initialise_commuters(map)
     vertex=map.station_name_index["Aldgate"]
     station_object=map.station_property_map[vertex]
     print station_object.number_of_current_commuters
     simulate_one_cycle(map)
-    vertex=map.station_name_index["Baker Street"]
+    vertex=map.station_name_index["Aldgate"]
     station_object=map.station_property_map[vertex]
     print station_object.number_of_current_commuters
     for vertex in map.graph_object.vertices():
         station_object=map.station_property_map[vertex]
         print station_object.name
         print station_object.number_of_current_commuters
+    
 
-main(map)
+#main(map)
+
+if __name__=="__main__":
+    #do nothing
+    main()
 
 
 
