9b89921ed6eed7b0d14d7bc0a81de6e6cd38d344
Winterflower <camillamon@gmail.com>
2017-04-04 21:59:54 +0100

    Adding data for Jubilee line simulation

---
+++ data/tubelines.py
@@ -0,0 +1,10 @@
+"""
+London tube stations by line
+"""
+
+TUBELINES = { 'Jubilee' : [ 'Stratford', 'West Ham', 'Canning Town', 'North Greenwich', 'Canary Wharf', \
+                'Canada Water', 'Bermondsey', 'London Bridge', 'Southwark', 'Waterloo', \
+                'Westminster', 'Green Park', 'Bond Street', 'Baker Street', 'St Johns Wood', \
+                'Swiss Cottage', 'Finchley Road', 'West Hampstead', 'Kilburn', 'Willesden Green', \
+                'Dollis Hill', 'Neasden', 'Wembley Park', 'Kingsbury', 'Queensbury', 'Cannons Park', \
+                'Stanmore'] }
