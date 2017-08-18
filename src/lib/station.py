30407efe468c6e9e9e5380fb0964f383a548335c
Winterflower <camillamon@gmail.com>
2017-08-18 15:04:40 +0100

    Board commuters based on the number of available places in the train

--- src/lib/station.py
+++ src/lib/station.py
@@ -4,6 +4,8 @@ Description: Library file that represents a Station object on the London undergr
 """
 import simpy
 from simpy.events import Event
+import numpy.random as nr
+import pdb
 
 class Station(object):
     def __init__(self, name, env):
@@ -16,6 +18,11 @@ class Station(object):
             print 'Station: Train arrived'
             self.board_commuters(trainobj)
     def board_commuters(self, trainobj):
-        for commuter in self.commuters:
-            trainobj.onboard_customer(commuter)
-        print 'Station: Finished onboarding customers'
+        if self.commuters:
+            remaining_space = min(trainobj.capacity - trainobj.space.count, len(self.commuters))
+            boarding_commuters = nr.choice(self.commuters, remaining_space, replace=False)
+            for commuter in boarding_commuters:
+                trainobj.onboard_customer(commuter)
+            print 'Station: Finished onboarding customers'
+        else:
+            print 'Empty station: {0}'.format(self.name)
